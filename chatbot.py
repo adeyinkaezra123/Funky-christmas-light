c528166a247f511d3fac79892d23781313685b07
Ezra Adeyinka <65364356+adeyinkaezra123@users.noreply.github.com>
2020-12-19 20:30:45 +0100

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
